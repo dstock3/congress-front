13a41f7eec9184daab089428afb96579bfeca14a
dstock3 <dstock3@gmail.com>
2022-09-10 17:24:16 -0500

    wrote basic fetch all to congress api. need to implement with key.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [bills, setBills] = useState(false)
+  
+  useEffect(()=> {
+    const fetchBills = (async () => {
+      try {
+        let res = await fetch(`https://api.congress.gov/v3/bill/`, {
+          method: "GET"
+        });
+
+        let resJson = await res.json();
+        if (res.status === 200) {
+          setBills(resJson.bills)
+
+        }
+        
+      } catch(err) {
+  
+      }
+
+    })();
+
+  
+  }, [])
+
   return (
     <div className="App">
-
+      {bills ? <div>{bills}</div> : null}
     </div>
   );
 }
